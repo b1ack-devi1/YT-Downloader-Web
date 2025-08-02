bc1eb1966dff69dab7f1ec8394a657e1df03b0c5
Mahdi <mh856155@gmail.com>
2025-08-02 13:08:18 +0600

    Initial commit

--- app.py
+++ app.py
@@ -57,4 +57,5 @@ def download():
     return send_file(file_path, as_attachment=True)
 
 if __name__ == "__main__":
-    app.run(debug=False)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
